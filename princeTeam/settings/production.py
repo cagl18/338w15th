a008e149bc41464fd7afdd4ede1e538000fcfadd
Carlos Gonzalez <cagl18@gmail.com>
2019-05-21 00:16:04 -0400

    change email settings

--- princeTeam/settings/production.py
+++ princeTeam/settings/production.py
@@ -17,8 +17,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['carlosgonzalez.pro']
 
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')#'Onetown Team <noreply@townresidential.com>'
+EMAIL_SUBJECT_PREFIX = '[338w15th.com] '
 EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-EMAIL_HOST= 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
