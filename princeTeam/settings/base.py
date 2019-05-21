7953dbdfcc4c494586b7f0417dccd3888eeee64f
Carlos Gonzalez <cagl18@gmail.com>
2019-05-21 00:22:23 -0400

    again

--- princeTeam/settings/base.py
+++ princeTeam/settings/base.py
@@ -38,10 +38,12 @@ ALLOWED_HOSTS = []
 # EMAIL_USE_TLS = True 
 # DEFAULT_FROM_EMAIL = 'noreply@townresidential.com'
 
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')#'Onetown Team <noreply@townresidential.com>'
+EMAIL_SUBJECT_PREFIX = '[338w15th] '
 EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-EMAIL_HOST= 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
--- princeTeam/settings/production.py
+++ princeTeam/settings/production.py
@@ -17,10 +17,16 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['carlosgonzalez.pro']
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')#'Onetown Team <noreply@townresidential.com>'
-EMAIL_SUBJECT_PREFIX = '[338w15th.com] '
+# EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+# EMAIL_HOST= 'smtp.sendgrid.net'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', cast=bool)
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')#'Onetown Team <noreply@townresidential.com>'
+EMAIL_SUBJECT_PREFIX = '[338w15th] '
 EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
